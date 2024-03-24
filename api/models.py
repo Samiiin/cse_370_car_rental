1b54c75b2debd6e813bcd566ddf1efd078ab18d7
Samin <saminyasarm@gmail.com>
2024-03-24 10:27:06 +0600

    I

--- api/models.py
+++ api/models.py
@@ -3,5 +3,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     # Add your custom fields here
-    bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    phone_number = models.IntegerField()
+    address = models.TextField()
+    email = models.EmailField()
+    is_admin = models.BooleanField(default=False)
+    user = models.ForeignKey(user,on_delete = models.CASCADE,related_name = "customers")
+
+class Car(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length= 200)
+    year = models.IntegerField()
+    base_rate= models.DecimalField(decimal_places = 2, max_digits= 6)
